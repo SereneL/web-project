4eb86688580cfd8c04de4deb26de710c0158dec5
SereneL <qoie200011@gmail.com>
2022-08-28 18:31:52 +0800

    add files

---
+++ data.js
@@ -0,0 +1,98 @@
+var film = ["荒唐分局",
+            "良善之地",
+            "千萬別抬頭",
+            "紙房子",
+            "D.P：逃兵追緝令",
+            "俄羅斯娃娃：派對迴旋",
+            "烏鴉足球隊",
+            "歡樂合唱團",
+            "米達斯之僕",
+            "陰謀辦公室",
+            ];
+var year = ["2013-2021","2016-2020","2021","2017-2021","2021","2019,2022","2015-2019","2009-2015","2020","2021"];            
+var pic = ["https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcOR11toWbKUOO_ibfUC7bsvqr8m1sJb361kc39sge9tX9uWLXF4v3GK8rhQCAcgzdsob9cIuTAROU103sXJBCgNRZEwQvditgs.webp?r=53a", 
+           "https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABfwtq-p6Gk_EVDhVhY5usCG9uwlUtJXz4vqe748uG-JqR0BzoEQfemyLdziNaw98tDNEX2DRcXmyngRAraXnbR4Rtb0nsODbrV4.webp?r=29f",
+           "https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABc8eXvsFr29CQ6dGXeKuPY7E1mGd18ivL_5o1thyc6fiMiJbtbjNcqdcZ-2KxxDX0RSEKLJ9cj2C6tUGr9RxZhSxKHMPx36eBLmKmNzFM9xTdS04lY7R9B8nKGyAnBlzOKjP.jpg?r=a00",
+           "https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVHuMicVdBCAOg2CDaQWyIA0Ts7RIkQSJrWME4rwQ8-So5hdPUPdRfACKgHzsSeyfYkKYZzG6B-KmyiUd-y0e7QM-EiyJqyuEu53vfllrXq-UUUUpm01jqkBK_q7ZX1aSgFl.jpg?r=e83",
+           "https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXzaB1bxAD9shTaJk1MNJK63wpIER9OvN6deLtunEHUsM7xG7mObZwz9mQL_TZD2BCmbLTdffWSt5CagKo4jSVn2ykqwh-OJPRGw3k2Tiaoe_GCbxskT9TbKZMytC1K1OT_g.jpg?r=3d1",
+           "https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABaIEaDocUI2EX2-44SvCN1AIapACpjr3Fy-0__ovxi9fMNXPghUHkUgzdST5EXEQfHIdw7fc1a-lFk7pTdbV31yQU8iuwJuH_jyBy8LAfS6mgINJVkaWCe_AXROK5u1DRRi7.jpg?r=6ae",
+           "https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQvPmqyy5L3XwJNCaPWQ0cJXLnBfX9eNjojJVM3xhTV3AcvTwVMvF2LkmtMmyaAzTNJzyUVVyewSQY0DpnyQkvcebGPLXa2wx57mIRoDvjkRPyTMDud0i6KRYRXFi_KuBPke.jpg?r=4db",
+           "https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcIlhkyI_Y029Irb-i6tgp_oyFROEReq8FZuhK99TfbVb3Wq7TUXgWD5qZqYFkVtN4oQeSHW2xqOtEuvchMh28TmvG4e0et5lhg.webp?r=6b2",
+           "https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcpv8D7dIDgwlDXmaRAlZ6v9hNzY3oN_JX-RfS6-Ietv0-dCOrGqjWgSrE4Mc6CDUpIojBh4QfmGPs_m6_uzhq5H3YoQmOlTkf3n6hqBr394vF6s0uQ5p4z3WYos2THrjg46.jpg?r=650",
+           "https://occ-0-3594-3996.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQmN2RZvMRXSCH1upzEv0AcWxFEo6OiD8n8fwKX3qiLYcUWt5PKOtv2isNzdArJOCtUpqXBPS27K8cBdZao7kF54Mjtht8-s0hn2GrpHVrCRyV4u_NTwNJ444Hp5DKxdcMHV.jpg?r=39e"];
+var link = ["https://www.netflix.com/title/70281562",
+            "https://www.netflix.com/title/80113701",
+            "https://www.netflix.com/title/81252357",
+            "https://www.netflix.com/title/80192098",
+            "https://www.netflix.com/title/81280917",
+            "https://www.netflix.com/tw/title/80211627",
+            "https://www.netflix.com/tw/title/80030346",
+            "https://www.netflix.com/tw/title/70143843",
+            "https://www.netflix.com/tw/title/81051782",
+            "https://www.netflix.com/tw/title/80995819"];
+var greeting = ["Hello",
+                "Labas",   //立陶宛
+                "Olá",     //葡萄牙
+                "Hola",    //西班牙
+                "Aloha",
+                "Zdrasti",   //保加利亞
+                "Zdravo",   //塞爾維亞
+                "Salut",    //法文
+                "Góðan daginn", //冰島
+                "Përshëndetje",  //阿爾巴尼亞
+                "Bok",       //克羅埃西亞
+                "Servus",   //德文
+                "G’day",
+                "Bună Ziua",  //羅馬尼亞
+                "Ni hao",
+                "Ahoy",  //英文
+                "Hej",   //瑞典
+                "Labdien",  //拉脫維亞
+                "Privet",   //俄羅斯
+                "Szia",      //匈牙利
+                "Hyvää päivää"  //芬蘭
+                ];
+
+var quiz_ans = [2, 3, 1, 0, 1, 0, 1, 1];
+var quiz_score = [20, 10, 10, 20, 10, 10, 10, 10];
+var question = ["▋ Which footballer showed up in the show?",
+                "▋ The team rescued ______ in season 3?",
+                "▋ Where did Bogota work before joining the team?",
+                "▋ Which character's child(ren) is/are not related to any kind of robbery?",
+                "▋ What is Stockholm's real name?",
+                "▋ Inspector Raquel's mother suffers from what medical condition?",
+                "▋ In which country does Raquel track the Professor after the first heist?",
+                "▋ 'Let the matriarchy begin' : Whose words are these?"];
+
+var opt1 = [" Lionel Messi",
+            " Cristiano Ronaldo",
+            " Neymar Jr.",
+            " David Beckham"];
+var opt2 = [" Tokyo",
+            " Denver",
+            " Lisbon",
+            " Rio"];
+var opt3 = [" Mine",
+            " Oil Platform",
+            " Reservoir",
+            " Power Plant"];
+var opt4 = [" Bogota",
+            " Benjamin",
+            " Moscow",
+            " Berlin"];
+var opt5 = [" Alicia",
+            "  Monica",
+            " Esther",
+            " Maria"];
+var opt6 = [" Alzheimer's Disease",
+            " Paranoid Schizophrenia",
+            " Post-Traumatic Stress Disorder",
+            " Cancer"];
+var opt7 = [" Philippines",
+            " Thailand",
+            " China",
+            " Malaysia"];
+var opt8 = [" Tokyo",
+            " Nairobi",
+            " Raquel",
+            " Alicia"];
